7725c8c31d363501b387bc76e3f4eb367d1c532f
SpotDylan <dylannguyen373@gmail.com>
2025-02-28 23:53:16 -0800

    added bash script to run on gpu 1

---
+++ run_on_gpu1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Force the use of GPU 1 by setting CUDA_VISIBLE_DEVICES
+# This makes GPU 1 appear as device 0 to the process
+export CUDA_VISIBLE_DEVICES=1
+echo "Set CUDA_VISIBLE_DEVICES=1 (GPU 1 will appear as device 0 to the process)"
+
+# Run the fine-tuning script with GPU ID 0 (which is actually GPU 1)
+python finetune_llada.py --gpu_id 0
